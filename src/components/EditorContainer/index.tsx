f6ac956c70f080ad59dcf9899fc40f93993f9d74
peachtang <951812856@qq.com>
2020-06-08 10:23:50 +0800

    init project

---
+++ src/components/EditorContainer/index.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './index.css'
+
+class EditorContainer extends Component {
+    render() {
+        return <div className="editor-wrap"></div>
+    }
+}
+
+export default EditorContainer
