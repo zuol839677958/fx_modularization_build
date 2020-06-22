0115b28da71a3af232c60b665f25f8fe0f7f3647
zuoliang <839677958@qq.com>
2020-06-22 17:50:43 +0800

    添加选择模板初始页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+import selectTemplatePage from './pages/templateSelect'
 import Home from './pages/Home'
 import Preview from './pages/Preview'
 
@@ -9,6 +10,7 @@ import './App.less'
 const App: FC = () => (
   <div className="App">
     <Router>
+      <Route exact path="/home" component={selectTemplatePage}></Route>
       <Route exact path="/" component={Home}></Route>
       <Route exact path="/preview" component={Preview}></Route>
     </Router>
---
+++ src/pages/templateSelect/index.tsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+
+
+import './index.less'
+
+interface ITemplateSelectProps {
+
+}
+
+class templateSelect extends Component<ITemplateSelectProps> {
+  render() {
+    
+
+    return (
+        <div className="template-select-box">
+            <div className="select-top">
+                <h2>请选择网站模板</h2>
+                <i className="iconfont close-btn">&#xE005;</i>
+            </div>
+            <div className="tmplate-content-list">
+                <div className="list-tmp-box">
+                    <ul>
+                        <li>
+                            <div className="tmp-left">
+                            <div className="img-box" >
+                                    <img src="https://imgs.wbp5.com/api/secrecymaster/html_up/2019/6/20190610115945561.png" alt=""/>
+                            </div>
+                                <div className="">
+                                    <span>预览</span><span>使用</span>    
+                                </div>    
+                            </div>    
+                            <div className="tmp-right">
+                                <h3>牛人榜模板1</h3>
+                                <p>模板说明：大图+图文+评论</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+  }
+}
+
+
+
+
+export default templateSelect
