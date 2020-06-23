8c3c2475be40ae0f8a23e42c5843a8bab9b38426
zuoliang <839677958@qq.com>
2020-06-23 14:46:47 +0800

    修改选择页

--- src/pages/templateSelect/index.tsx
+++ src/pages/templateSelect/index.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { Pagination } from 'antd';
 
 import './index.less'
 
@@ -9,7 +9,7 @@ interface ITemplateSelectProps {
 
 class templateSelect extends Component<ITemplateSelectProps> {
   render() {
-    
+        
 
     return (
         <div className="template-select-box">
@@ -30,15 +30,88 @@ class templateSelect extends Component<ITemplateSelectProps> {
                                 </div>    
                             </div>    
                             <div className="tmp-right">
-                                <h3>牛人榜模板1</h3>
+                                <h6>牛人榜模板1</h6>
+                                <p>模板说明：大图+图文+评论</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="tmp-left">
+                                <div className="img-box" >
+                                        <img src="https://imgs.wbp5.com/api/secrecymaster/html_up/2019/6/20190610115945561.png" alt=""/>
+                                </div>
+                                <div className="preview-usered">
+                                    <span className="preview">预览</span><span className="usered" >使用</span>    
+                                </div>    
+                            </div>    
+                            <div className="tmp-right">
+                                <h6>牛人榜模板1</h6>
+                                <p>模板说明：大图+图文+评论</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="tmp-left">
+                                <div className="img-box" >
+                                        <img src="https://imgs.wbp5.com/api/secrecymaster/html_up/2019/6/20190610115945561.png" alt=""/>
+                                </div>
+                                <div className="preview-usered">
+                                    <span className="preview">预览</span><span className="usered" >使用</span>    
+                                </div>    
+                            </div>    
+                            <div className="tmp-right">
+                                <h6>牛人榜模板1</h6>
+                                <p>模板说明：大图+图文+评论</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="tmp-left">
+                                <div className="img-box" >
+                                        <img src="https://imgs.wbp5.com/api/secrecymaster/html_up/2019/6/20190610115945561.png" alt=""/>
+                                </div>
+                                <div className="preview-usered">
+                                    <span className="preview">预览</span><span className="usered" >使用</span>    
+                                </div>    
+                            </div>    
+                            <div className="tmp-right">
+                                <h6>牛人榜模板1</h6>
+                                <p>模板说明：大图+图文+评论</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="tmp-left">
+                                <div className="img-box" >
+                                        <img src="https://imgs.wbp5.com/api/secrecymaster/html_up/2019/6/20190610115945561.png" alt=""/>
+                                </div>
+                                <div className="preview-usered">
+                                    <span className="preview">预览</span><span className="usered" >使用</span>    
+                                </div>    
+                            </div>    
+                            <div className="tmp-right">
+                                <h6>牛人榜模板1</h6>
                                 <p>模板说明：大图+图文+评论</p>
                             </div>
                         </li>
                     </ul>
+                   
                 </div>
+                <div className="tip-pagination">
+                    <Pagination
+                        showQuickJumper
+                        size="small"
+                        onShowSizeChange={this.onShowSizeChange}
+                        defaultCurrent={1}
+                        hideOnSinglePage={true}
+                        pageSize={8}
+                        pageSizeOptions={["8"]}
+                        total={9}
+                        />
+
+                    </div>
             </div>
         </div>
     )
+  }
+  onShowSizeChange(current:any,pageSize:any){
+
   }
 }
 
