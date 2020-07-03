d9f6cd2b9356dcdc5e84ffe671d148fd762d6645
peachtang <951812856@qq.com>
2020-07-03 14:54:36 +0800

    配置环境

--- src/axios/env.ts
+++ src/axios/env.ts
@@ -1,25 +1,31 @@
 export enum EnvTpye {
-  Test, // 测试环境
+  Test = 1, // 测试环境
   Pre, // 预发布
   Product // 正式环境
 }
 
-export enum RequestUrl {
-  'https://testmswebapi.tostar.top',
-}
-
-export enum SpecialLinkUrl {
-  '',
-}
-
-export const env = EnvTpye.Test
+const env: EnvTpye = 1
 
 const getEnvRequestUrl = () => {
-  return RequestUrl[env]
+  switch (env) {
+    case EnvTpye.Test:
+      return 'https://testmswebapi.tostar.top'
+    case EnvTpye.Pre:
+      return 'https://premswebapi.tostar.top'
+    case EnvTpye.Product:
+      return 'https://gamswebapi.wbp5.com'
+  }
 }
 
 const getSepecialLinkUrl = () => {
-  return SpecialLinkUrl[env]
+  switch (env) {
+    case EnvTpye.Test:
+      return ''
+    case EnvTpye.Pre:
+      return ''
+    case EnvTpye.Product:
+      return ''
+  }
 }
 
 export {
