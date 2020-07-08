6878f9ad881c7e5dd93cd235521be7736d4d332a
zuoliang <839677958@qq.com>
2020-07-08 11:05:00 +0800

    分享功能添加

--- src/pages/Preview/index.tsx
+++ src/pages/Preview/index.tsx
@@ -9,6 +9,20 @@ interface IPreviewProps {
 }
 
 class Preview extends Component<IPreviewProps> {
+  componentDidMount(){
+    let s:Element = document.querySelector(".share_box")!;
+    let script = document.createElement("script");
+    script.src = 'https://js.wbp5.com/script/public/jquery/jquery-1.8.3.min.js';
+    s.appendChild(script);
+    let script2 = document.createElement("script");
+    script2.src = 'https://js.wbp5.com/script/public/Bshare/bshare.min.js';
+    s.appendChild(script2);
+    let script1 = document.createElement("script");
+    script1.src = 'https://js.wbp5.com/script/public/QRCode/generate.min.js';
+    s.appendChild(script1);
+ }
+
+
   render() {
     try {
       const pageData = JSON.parse(localStorage.getItem('pageEditorData') as string) as IPageModel
--- src/template/Share/index.tsx
+++ src/template/Share/index.tsx
@@ -7,12 +7,24 @@ import './index.less'
 
 interface IShareProps extends IMasterTemplateProps { }
 
-interface ICorrelationSpecialState extends IMasterTemplateState { }
+interface IShareState extends IMasterTemplateState { }
 
 class Share extends MasterTemplate<IShareProps> {
-  state: ICorrelationSpecialState = {
+  state: IShareState = {
     isShowMask: false
   }
+  componentDidMount(){
+    let s:Element = document.querySelector(".share_box")!;
+    let script = document.createElement("script");
+    script.src = 'https://js.wbp5.com/script/public/jquery/jquery-1.8.3.min.js';
+    s.appendChild(script);
+    let script2 = document.createElement("script");
+    script2.src = 'https://js.wbp5.com/script/public/Bshare/bshare.min.js';
+    s.appendChild(script2);
+    let script1 = document.createElement("script");
+    script1.src = 'https://js.wbp5.com/script/public/QRCode/generate.min.js';
+    s.appendChild(script1);
+ }
 
   render() {
     const {
@@ -55,7 +67,28 @@ class Share extends MasterTemplate<IShareProps> {
       >
         {this.renderMask(maskParams)}
         {/* {this.renderShareTmp(tempData.tempData as ICorrelationSpecialModel[])} */}
-        1111
+        <div className="share_content">
+            <div className="mainland_share">
+              <span className="share">
+                    <em>分享给朋友：</em>
+                    <i className="qq_div1 weixin bg_i" title="微信" data-bshare='{type:"weixin",url:"fx110.com",title:"111",summary:"111",images:""}'></i>
+                    <i className="qq_div2 weibo bg_i" title="微博" data-bshare='{type:"weibo",url:"",title:"",summary:"（分享至@FX110）",images:""}'></i>
+                    <i className="qq_div3 qzone bg_i" title="QQ空间" data-bshare='{type:"qzone",url:"",title:"",summary:"",images:""}'></i>
+              </span>
+              
+            </div>
+            {/* <div className="tw_share">
+                <span className="share">
+                      <em>分享連結：</em>
+                      <i className="qq_div1 line bg_i" title="line" data-bshare='{type:"line",url:"",title:"",summary:"",images:""}'></i>
+                      <i className="qq_div2 facebook bg_i" title="facebook" data-bshare='{type:"facebook",url:"",title:"",summary:"（分享至@FX110）",images:""}'></i>
+                      <i className="qq_div3 twitter bg_i" title="twitter" data-bshare='{type:"twitter",url:"",title:"",summary:"",images:""}'></i>
+                  </span> */}
+                  {/* <script src="~/Scripts/SharedViewmin/twShare.js?v=ded726c262"></script> */}
+              {/* </div> */}
+
+          </div>
+          
       </div>
     )
   }
