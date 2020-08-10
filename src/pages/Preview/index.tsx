7b4b098361bf78304201f94f64a64b2edef686fb
zuoliang <839677958@qq.com>
2020-08-10 19:46:35 +0800

    移动端预览部分功能效果添加

--- src/pages/Preview/index.tsx
+++ src/pages/Preview/index.tsx
@@ -1,19 +1,23 @@
 import React, { PureComponent } from 'react'
 import { IPageModel } from '../../store/data'
 import { message } from 'antd'
-
 import './index.less'
 import { MobileTwoTone, FundProjectionScreenOutlined } from '@ant-design/icons'
-
+import QRCode from 'qrcode.react'
+import CopyToClipboard from 'react-copy-to-clipboard';
 interface IPreviewProps {
   pageHtml: string
 }
-interface IPreviewType{
-    isWeb:boolean
+interface IPreviewType {
+  isWeb: boolean
+  preUrl: string
+  copied: boolean
 }
-class Preview extends PureComponent<IPreviewProps,IPreviewType> {
+class Preview extends PureComponent<IPreviewProps, IPreviewType> {
   state: IPreviewType = {
-    isWeb: true
+    isWeb: false,
+    preUrl: "https://fx110.uk",
+    copied: false
   }
 
   componentDidMount() {
@@ -33,35 +37,61 @@ class Preview extends PureComponent<IPreviewProps,IPreviewType> {
       script4.src = 'https://www.fx110.com.tw/Scripts/SharedViewmin/twShare.js'
       ele.appendChild(script4)
     }
+
   }
 
   render() {
     try {
       const pageData = JSON.parse(localStorage.getItem('pageEditorData') as string) as IPageModel
       const { pageHtml } = pageData
-     
+
       return (
         <div className="preview_content">
           {
-          this.state.isWeb?
-            <section className="preview-wrap"  dangerouslySetInnerHTML={{ __html: pageHtml }}></section>
-          : <section className="preview-mobile">
-                  <div className="mobile-content">
-                        <div className="editor-wrap">
-                            <div className="mobile-wrap">
-                              <iframe src="http://mtestolv1.tostar.top/special/3197" title="专题" style={{border:0,width:375,height:660}} frameBorder="0" ></iframe>
-                              {/* <iframe src="https:m.baidu.com" title="专题" style={{border:0,width:370,height:660}} frameBorder="0" ></iframe> */}
-
-                            </div>
-                        </div>
+            this.state.isWeb ?
+              <section className="preview-wrap" dangerouslySetInnerHTML={{ __html: pageHtml }}></section>
+              : <section className="preview-mobile">
+                <div className="mobile-content">
+                  <div className="editor-wrap">
+                    <div className="mobile-wrap">
+                      <iframe src="http://mtestolv1.tostar.top/special/3197" title="专题" style={{ border: 0, width: 375, height: 660 }} frameBorder="0" ></iframe>
+                      {/* <iframe src="https:m.baidu.com" title="专题" style={{border:0,width:370,height:660}} frameBorder="0" ></iframe> */}
+                    </div>
                   </div>
-            </section>
+                  <div className="qrcode_copy_url">
+                    <div className="qrcode">
+                      <h3>微信扫一扫，看效果</h3>
+                      <div className="qr_box">
+                        <QRCode
+                          value={this.state.preUrl}  //value参数为生成二维码的链接
+                          size={140} //二维码的宽高尺寸
+                          fgColor="#000000"  //二维码的颜色
+                        />
+                      </div>
+
+                    </div>
+                    <div className="copy_url">
+                      <input value={this.state.preUrl}
+                        onChange={this.handleInputChange} />
+                      <div className="copy_box">
+                        <CopyToClipboard  text={this.state.preUrl}
+                          onCopy={() => this.setState({ copied: true })}>
+                          <button onClick={() => this.tipMessage()}> 复制链接 </button>
+                        </CopyToClipboard>
+                      </div>
+
+                    </div>
+                  </div>
+
+                </div>
+
+              </section>
           }
           <section className="preview-bottom-bar">
-              <div className="Mobile_box">
-                <FundProjectionScreenOutlined onClick={ ()=>{ this.changeToWeb()} } style={{ fontSize:24,marginRight:10,color:"blue"}} /> 
-                <MobileTwoTone onClick={ ()=>{ this.changeToMoblie()} } twoToneColor="#ddd" style={{ fontSize:24 }}/> 
-              </div>
+            <div className="Mobile_box">
+              <FundProjectionScreenOutlined onClick={() => { this.changeToWeb() }} style={{ fontSize: 24, marginRight: 10, color: "blue" }} />
+              <MobileTwoTone onClick={() => { this.changeToMoblie() }} twoToneColor="#ddd" style={{ fontSize: 24 }} />
+            </div>
           </section>
         </div>
       )
@@ -70,16 +100,25 @@ class Preview extends PureComponent<IPreviewProps,IPreviewType> {
       return null
     }
   }
+  handleInputChange = (e: any) => {
+    // this.setState({
+    //   preUrl:e.target.value
+    // })
+
+  }
+  tipMessage = () => {
+    message.success("复制成功")
+  }
   changeToWeb = () => {
     this.setState({
-       isWeb:true
+      isWeb: true
     })
 
   }
-  changeToMoblie =() =>{
+  changeToMoblie = () => {
     this.setState({
-      isWeb:false
-   })
+      isWeb: false
+    })
   }
 
 }
