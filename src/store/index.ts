def1e8bab3cc730bfaf99b8d80dd201b64312da3
peachtang <951812856@qq.com>
2020-09-27 16:42:51 +0800

    update

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,15 +3,11 @@ import { createStore, applyMiddleware, compose, PreloadedState } from 'redux'
 import reducers from './reducers'
 import initialState from './state'
 import thunk from 'redux-thunk'
+
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(reducers, initialState as PreloadedState<any>, composeEnhancers(
   applyMiddleware(thunk)
 ))
-
-console.log('reducers', reducers)
-console.log('state', initialState)
-
-console.log(store.getState())
 
 export default store
